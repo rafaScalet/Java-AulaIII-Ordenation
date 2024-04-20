2467046c0965c5a57bdbbadd39a2e47bd9e5fb88
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 12:42:58 -0300

    recognize the bigger number

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,28 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int a, b, c, bigger;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the numbers: ");
+
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+        in.close();
+
+        bigger = a;
+
+        if (b > bigger && b > c) {
+            bigger = b;
+        } else if (c > bigger) {
+            bigger = c;
+        }
+
+        System.out.println("bigger: " + bigger);
     }
 }
